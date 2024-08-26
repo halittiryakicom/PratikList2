a2a8b37d175b9f4dccbe40343c3382b45a8f68fc
Halit TİRYAKİ <halittiryaki61@outlook.com>
2024-08-26 13:16:01 +0300

    Add project files.

---
+++ PratikList2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Liste oluşturuldu.
+List <string> coffes = new List<string>();
+
+//kullanıcıdan 5 adet kahve isimi alınıp listeye eklendi
+for  (int i = 0;i <5;i++)
+{
+    Console.Write($"Kahve {i+1}:");
+    coffes.Add(Console.ReadLine());
+}
+
+//kahve isimleri yazdırıldı.
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("Girilen Kahve İsimleri");
+foreach(string item  in coffes)
+{
+    Console.WriteLine(item);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#Liste Pratiði 2
